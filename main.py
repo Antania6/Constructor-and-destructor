412a5a30e3e75b5d1f80edaa9a277ad9452376c8
Antania6 <antania.baset@gmail.com>
2024-09-05 19:32:55 +0500

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+class employee :
+    
+    def __init__(self):
+        print("employee created")
+
+
+    def __del__(self):
+        print("destructor called, employee deleted")
+
+
+obj = employee()
+del obj
 print("hello world")
+input("enter a number")
